d78e659bc21b80570019fb2ff6d3180e4022fc78
yysun <yeyi.sun@futurmaster.com>
2015-06-25 15:27:41 +0800

    commit missing files.

---
+++ FMLogDemo/FMLog/FMLogNamespaceMacro.h
@@ -0,0 +1,31 @@
+﻿///@file FMLogNamespaceMacro.h
+/// <summary>
+///   
+/// </summary>
+/// -------------------------------------------------------------------------<BR>
+/// <description>
+/// <BR>
+/// </description>
+/// <BR>
+/// Created By: yysun <BR>
+/// Creation Date: Friday June 19, 2015 <BR> 
+/// Reviewed By: <BR>
+/// Review Date: <BR>
+/// -------------------------------------------------------------------------<BR>
+///@addtogroup FMLog FMLog
+///@{
+
+#ifndef FMLogNamespaceMacro_h__
+#define FMLogNamespaceMacro_h__
+
+#define  _FMLogNamespaceBegin   
+#define  _FMLogNamespaceEnd     
+#define  _FMLogNamespace        
+
+//#define  _FMLogNamespaceBegin   namespace fmlog {
+//#define  _FMLogNamespaceEnd     }
+//#define  _FMLogNamespace        ::fmlog::
+
+#endif // FMLogNamespaceMacro_h__
+
+///@}//end of addtogroup FMLog FMLog
